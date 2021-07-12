719a3e881dc65c43979195019b790c1d3a60311d
Omar Gómez <omar.gomez@bnamericas.com>
2021-07-11 23:31:56 -0400

    add custom configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import WordCounter from "./components/WordCounter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WordCounter />
   </React.StrictMode>,
   document.getElementById('root')
 );
